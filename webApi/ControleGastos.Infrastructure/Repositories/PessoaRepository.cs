da16fa5e4ea1c645ae66935a69d0c5f2f4bc61b9
caiowmp <contato.caiomp@gmail.com>
2026-04-05 19:42:03 -0300

    feat: implementa os repositories de cada entidade

---
+++ webApi/ControleGastos.Infrastructure/Repositories/PessoaRepository.cs
@@ -0,0 +1,34 @@
+﻿using ControleGastos.Application.Interfaces.Repositories.Pessoa;
+using ControleGastos.Domain.Entities;
+using ControleGastos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Infrastructure.Repositories
+{
+  internal class PessoaRepository(ControleGastosDbContext _dbContext) : IPessoaReadOnly, IPessoaWriteOnly, IPessoaUpdateOnly
+  {
+    public async Task Add(Pessoa pessoa)
+    {
+      await _dbContext.Pessoas.AddAsync(pessoa);
+    }
+
+    public async Task Delete(long id)
+    {
+      var result = await _dbContext.Pessoas.FindAsync(id);
+
+      _dbContext.Pessoas.Remove(result!);
+    }
+
+    public async Task<List<Pessoa>> GetAll()
+    {
+      return await _dbContext.Pessoas
+        .AsNoTracking()
+        .ToListAsync();
+    }
+
+    public void Update(Pessoa pessoa)
+    {
+      _dbContext.Pessoas.Update(pessoa);
+    }
+  }
+}
