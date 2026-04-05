b616449b12ab2e0d59ed2448ebe892eab4b634aa
caiowmp <contato.caiomp@gmail.com>
2026-04-05 19:24:45 -0300

    feat: adiciona dbcontext

---
+++ webApi/ControleGastos.Infrastructure/Data/ControleGastosDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ControleGastos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Infrastructure.Data
+{
+  public class ControleGastosDbContext(DbContextOptions<ControleGastosDbContext> options) : DbContext(options)
+  {
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Pessoa>()
+          .HasMany(p => p.Transacoes)
+          .WithOne()
+          .HasForeignKey(t => t.PessoaId)
+          .OnDelete(DeleteBehavior.Cascade);
+    }
+  }
+}
+
